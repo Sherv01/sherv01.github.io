b9a631b41d78c909c3daa49c3a37c534ba72f02d
Shervin Darmanki Farahani <155998454+Sherv01@users.noreply.github.com>
2024-02-28 16:50:11 -0500

    Update gitprofile.tsx

--- src/components/gitprofile.tsx
+++ src/components/gitprofile.tsx
@@ -261,7 +261,7 @@ const GitProfile = ({ config }: { config: Config }) => {
                       />
                     )}
                   </div>
-                </div> 
+                </div>
               <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 rounded-box">
                 <div className="lg:col-span-3 col-span-1">
                   <div className="grid grid-cols-1 gap-6">
